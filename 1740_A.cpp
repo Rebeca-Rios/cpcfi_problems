5845e8fbf4f3a4c750865b3adf2802062d94be61
Rebeca-Rios <80608872+Rebeca-Rios@users.noreply.github.com>
2022-11-10 16:07:16 -0600

    Add files via upload

---
+++ 1740_A.cpp
@@ -0,0 +1,19 @@
+    /*
+    	1740A - Factorise N+M
+    	https://codeforces.com/problemset/problem/1740/A
+    */
+     
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main(){
+    	int t;	cin>>t;
+    	for(int i=0; i<t; i++){
+    		int n;	cin>>n;
+            if(n == 2)
+               cout<<2<<endl;
+            else
+    		   cout<<3<<endl;
+    	}
+    	return 0;
+    }
